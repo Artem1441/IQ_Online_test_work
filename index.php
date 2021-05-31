b0ac465c5a15d3ba7065cb3d72e8755fd8a1c1be
Artem1441 <74830063+Artem1441@users.noreply.github.com>
2021-05-31 16:20:23 +0500

    Update index.php

--- index.php
+++ index.php
@@ -10,8 +10,7 @@
       <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
       <link rel="preconnect" href="https://fonts.gstatic.com">
       <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
-      <link rel="stylesheet" href="css/styles.css">
-      <link rel="stylesheet" href="css/media.css">
+      <link rel="stylesheet" href="styles.css">
 
       <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
       <script src="//ajax.aspnetcdn.com/ajax/jquery.ui/1.10.3/jquery-ui.min.js"></script>
